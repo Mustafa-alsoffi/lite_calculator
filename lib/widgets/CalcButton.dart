2068a43f8a75c8368d94d81e76c93343d5683e52
Mustafa_alsoffi <alsoffimustafa@gmail.com>
2021-08-03 00:07:19 +0800

    Buttons layout resized to smaller

--- lib/widgets/CalcButton.dart
+++ lib/widgets/CalcButton.dart
@@ -11,20 +11,20 @@ class CalcButton extends StatelessWidget {
     this.fillColor,
     required this.text,
     this.textColor = 0xFFFFFFFF,
-    this.textSize = 24.00,
+    this.textSize = 20.00,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.all(6),
+      margin: EdgeInsets.all(7),
       child: SizedBox(
-        width: 65,
-        height: 65,
+        width: 45,
+        height: 45,
         child: TextButton(
           child: Text(text,
               style: GoogleFonts.rubik(
-                  textStyle: TextStyle(fontSize: 24), fontSize: textSize)),
+                  textStyle: TextStyle(fontSize: 10), fontSize: textSize)),
           onPressed: () {},
           style: TextButton.styleFrom(
               backgroundColor: fillColor != null ? Color(fillColor!) : null,
